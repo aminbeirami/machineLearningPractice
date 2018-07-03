9cea9052acedfdc4efa79b4cf9c4d50f69857223
Amin Beirami <beirami.m.a@gmail.com>
2018-07-03 12:23:56 -0400

    polynomial regression editted

--- linear-Regression/03.polynomial regression/polynomial_regression.py
+++ linear-Regression/03.polynomial regression/polynomial_regression.py
@@ -5,13 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 #reading the dataset
-dataset = pd.read_csv('Data/data.csv')
-dataset = dataset.sort_values(by = ['brain_weight'])
-X = dataset.iloc[:,1:2]
-y = dataset.iloc[:,2]
-print y
+dataset = pd.read_csv('Data/Data2.csv')
+dataset = dataset.sort_values(by = ['interest rate (%)'])
+X = (dataset['interest rate (%)']).values.reshape(-1,1)
+y = [float((rec.strip('$')).replace(',','')) for rec in dataset['Median home price']]
+
 # fitting polynomial regression to the dataset
-poly_regression = PolynomialFeatures(degree = 12) # this adds another column(s) to the dataset
+poly_regression = PolynomialFeatures(degree = 6) # this adds another column(s) to the dataset
 X_polynomial = poly_regression.fit_transform(X) # we need to fit and transform newly made dataset into X_pol variable
 poly_regression.fit(X_polynomial,y)
 regressor = LinearRegression()
@@ -19,4 +19,4 @@ regressor.fit(X_polynomial,y)
 
 plt.scatter(X,y,color = 'red')
 plt.plot(X, regressor.predict(poly_regression.fit_transform(X)),color = 'blue')
-plt.show()
+plt.savefig('plynomial_linear_regression.png')
